46127bf32d504a98bf8d7329e4e9cfae3f97b2fa
David Helmus <david@helmus.me>
2026-09-16 19:39:45 +0200

    chore: pin 0.2.0 release source checksum

--- packaging/install.sh
+++ packaging/install.sh
@@ -19,7 +19,7 @@ VERSION="0.2.0"
 # This release asset is built from the verified application tree so
 # Super+Space opens an empty editor without requiring a git clone.
 TARBALL_URL="https://github.com/Skeptomenos/omapreview/releases/download/v${VERSION}/omapreview-${VERSION}-src.tar.gz"
-TARBALL_SHA256="6f0c985d05915d14e034db18f40175998e8d53f4d1cab26d6e7a2b52f2f2436a"
+TARBALL_SHA256="9c5dcfd5e724c7063fd401b576c599feed0f500426a55e24d45d1f173e994d91"
 PREFIX="${HOME}/.local/share/omapreview"
 BIN_DIR="${HOME}/.local/bin"
 APP_DIR="${HOME}/.local/share/applications"
